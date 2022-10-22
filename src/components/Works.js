0b554979a2546557b7d8da4486bce225874b2a20
Achilles-Dev <solohag92@gmail.com>
2022-10-22 13:30:49 +0000

    Update Works component

--- src/components/Works.js
+++ src/components/Works.js
@@ -12,17 +12,21 @@ const Works = () => {
         <div className=" flex justify-center">
           <h1 className="text-4xl p-3 border-0 border-b-2 border-b-green">My Recent Works</h1>
         </div>
-        <ul className="my-16">
+        <ul className="my-16 flex flex-col gap-5">
           { projects.map((project) => (
             project.id <= 3
               ? (
-                <li key={project.id} className="grid grid-cols-12 gap-2 mb-7 relative">
-                  <div className="flex flex-col gap-4 relative col-[7_/_-1] row-[1_/_-1] text-right">
-                    <h2 className="flex justify-end text-3xl">{project.name}</h2>
-                    <div className="bg-ligth-navy py-5 px-4 z-20 min-h-fit text-slate-400">
+                <li key={project.id} className="grid grid-cols-12 gap-2 mb-7 relative p-5 items-center">
+                  <div className={`flex flex-col gap-4 p-7 md-p-0 relative col-[1_/_-1] ${project.id % 2 === 0 ? 'md:col-[1_/_9] lg:col-[1_/_7]' : 'md:col-[5_/_-1] lg:col-[7_/_-1]'} row-[1_/_-1] z-20`}>
+                    <h2 className={`flex justify-start ${project.id % 2 === 0 ? 'md:justify-start' : 'md:justify-end'} text-3xl`}>
+                      <a href={project['live-version-link']['link-href']} target="_blank" rel="noopener noreferrer">
+                        {project.name}
+                      </a>
+                    </h2>
+                    <div className="md:bg-ligth-navy py-5 md:px-4 min-h-fit text-slate-400">
                       <p>{project.description}</p>
                     </div>
-                    <ul className="flex gap-2 flex-wrap justify-end text-slate-400">
+                    <ul className={`flex gap-2 flex-wrap justify-start ${project.id % 2 === 0 ? 'md:justify-start' : 'md:justify-end'} text-slate-400"`}>
                       {project.technologies.map((tech) => (
                         <li key={tech}>{tech}</li>
                       ))}
@@ -36,7 +40,7 @@ const Works = () => {
                       </a>
                     </div>
                   </div>
-                  <div className="h-52 border relative col-[1_/_8] row-[1_/_-1] z-10 bg-green">
+                  <div className={`h-80 opacity-25 md:opacity-100 bg-slate-200 relative col-[1_/_-1] ${project.id % 2 === 0 ? 'md:col-[6_/_-1]' : 'md:col-[1_/_8]'} row-[1_/_-1] z-10`}>
                     <a className="work-image" href={project['live-version-link']['link-href']} target="_blank" rel="noopener noreferrer">
                       <img className="work-image" src="" alt={project.name} />
                     </a>
