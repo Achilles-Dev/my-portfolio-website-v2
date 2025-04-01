f6634a932fc016c47fd3cc1e04908423dd5775bb
Achilles <solohag92@gmail.com>
2025-04-01 11:52:48 +0000

    Add new project

--- src/components/About.js
+++ src/components/About.js
@@ -3,30 +3,67 @@ import Profile from '../assets/images/profile_image.jpeg';
 
 const About = () => (
   <section className="px-8 py-12 md:p-14 lg:p-24 text-slate-300" id="about">
-    <div className="wow pulse flex justify-center mb-5 p-3" data-wow-delay="1s" data-wow-duration="2s">
-      <h2 className="text-4xl p-3 border-0 border-b-2 border-b-green">About me</h2>
+    <div
+      className="wow pulse flex justify-center mb-5 p-3"
+      data-wow-delay="1s"
+      data-wow-duration="2s"
+    >
+      <h2 className="text-4xl p-3 border-0 border-b-2 border-b-green">
+        About me
+      </h2>
     </div>
     <div className="grid grid-cols-12 gap-3">
-      <div className="wow fadeInUp flex md:items-center opacity-5 md:opacity-100 col-[_1/_-1] md:col-[_1/_7] row-[_1/_-1] md:row-[_1/_5] w-full h-full" data-wow-delay="1s" data-wow-duration="2s">
+      <div
+        className="wow fadeInUp flex md:items-center opacity-5 md:opacity-100 col-[_1/_-1] md:col-[_1/_7] row-[_1/_-1] md:row-[_1/_5] w-full h-full"
+        data-wow-delay="1s"
+        data-wow-duration="2s"
+      >
         <img src={Profile} alt="Solomon Hagan" />
       </div>
       <div className="col-[_1/_-1] md:col-[_7/_-1] row-[_1/_-1] p-3 h-full">
         <div className="flex flex-col gap-5 h-full justify-center md:justify-start">
           <div>
-            <h2 className="wow fadeInDown text-2xl text-center md:text-start" data-wow-delay="1s" data-wow-duration="2s">Self-taught Full Stack Developer</h2>
+            <h2
+              className="wow fadeInDown text-2xl text-center md:text-start"
+              data-wow-delay="1s"
+              data-wow-duration="2s"
+            >
+              Self-taught Full Stack Developer
+            </h2>
           </div>
-          <p className="wow fadeInDown text-md text-center md:text-start" data-wow-delay="1s" data-wow-duration="2s">
-            I can help you build a product,
+          <p
+            className="wow fadeInDown text-md text-center md:text-start"
+            data-wow-delay="1s"
+            data-wow-duration="2s"
+          >
+            {/* I can help you build a product,
             feature or website. Look through some of my work and experience!
             If you like what you see and have a project you need coded,
-            don’t hestiate to contact me.
+            don’t hestiate to contact me. */}
+            {`I'm a passionate freelance full-stack developer with a knack for
+            crafting seamless, user-friendly web experiences. While I thrive on
+            tackling both frontend and backend challenges, my heart leans toward
+            the frontend—where I bring designs to life with clean code,
+            intuitive interfaces, and a keen eye for detail. With expertise in
+            modern JavaScript frameworks, responsive design, and a solid
+            foundation in server-side technologies, I deliver end-to-end
+            solutions tailored to my clients' needs. Whether it’s building
+            dynamic applications or optimizing performance, I’m driven by a love
+            for problem-solving and creating digital experiences that delight
+            and inspire.`}
           </p>
           <div className="flex flex-col gap-3">
-            <h3>Technologies I have been working with:</h3>
-            <ul className="wow rollIn flex flex-wrap gap-2 justify-center md:justify-start" data-wow-delay="1s" data-wow-duration="2s">
+            <h3>Technologies I work with:</h3>
+            <ul
+              className="wow rollIn flex flex-wrap gap-2 justify-center md:justify-start"
+              data-wow-delay="1s"
+              data-wow-duration="2s"
+            >
               <li className="border p-3 border-green">JavaScript</li>
+              <li className="border p-3 border-green">TypeScript</li>
               <li className="border p-3 border-green">React</li>
               <li className="border p-3 border-green">Redux</li>
+              <li className="border p-3 border-green">Next.js</li>
               <li className="border p-3 border-green">Ruby</li>
               <li className="border p-3 border-green">Ruby on Rails</li>
               <li className="border p-3 border-green">Bootstrap</li>
--- src/components/Works.js
+++ src/components/Works.js
@@ -6,12 +6,19 @@ import folderIcon from '../assets/icons/folder.svg';
 import resultsAnalyser from '../assets/images/results-analyser.png';
 import recipeApp from '../assets/images/recipe-app.png';
 import bookstore from '../assets/images/bookstore.png';
+import restaurantApp from '../assets/images/restaurant.png';
 
 const Works = () => {
   const projects = projectDetails;
   const [images, setImages] = useState([]);
   useEffect(() => {
-    setImages([...images, resultsAnalyser, recipeApp, bookstore]);
+    setImages([
+      ...images,
+      restaurantApp,
+      resultsAnalyser,
+      recipeApp,
+      bookstore,
+    ]);
   }, []);
 
   return (
@@ -27,96 +34,106 @@ const Works = () => {
           </h1>
         </div>
         <ul className="my-16 flex flex-col gap-5">
-          {projects.map((project) => (project.id <= 3 ? (
-            <li
-              key={project.id}
-              className="wow fadeInUp grid grid-cols-12 gap-2 mb-7 relative items-center"
-              data-wow-delay="0.5s"
-              data-wow-duration="2s"
-            >
-              <div
-                className={`flex flex-col gap-4 p-7 md-p-0 relative col-[1_/_-1] ${
-                  project.id % 2 === 0
-                    ? 'md:col-[1_/_9] lg:col-[1_/_7]'
-                    : 'md:col-[5_/_-1] lg:col-[7_/_-1]'
-                } row-[1_/_-1] z-20`}
+          {projects
+            .filter((project) => project.id <= 4)
+            .map((project) => (
+              <li
+                key={project.id}
+                className="wow fadeInUp grid grid-cols-12 gap-2 mb-7 relative items-center"
+                data-wow-delay="0.5s"
+                data-wow-duration="2s"
               >
-                <h2
-                  className={`flex justify-start ${
-                    project.id % 2 === 0
-                      ? 'md:justify-start'
-                      : 'md:justify-end'
-                  } text-3xl`}
-                >
-                  <a
-                    href={project['live-version-link']['link-href']}
-                    className="hover:text-green focus:text-green "
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {project.name}
-                  </a>
-                </h2>
-                <div className="md:bg-ligth-navy py-5 md:px-4 min-h-fit text-slate-400">
-                  <p>{project.description}</p>
-                  {project.id === 1 ? <span className="mt-3 text-sky-500">Login with Username: solomon@gmail.com and password: admin123</span> : ''}
-                </div>
-                <ul
-                  className={`flex gap-2 flex-wrap justify-start ${
-                    project.id % 2 === 0
-                      ? 'md:justify-start'
-                      : 'md:justify-end'
-                  } text-slate-400"`}
-                >
-                  {project.technologies.map((tech) => (
-                    <li key={tech}>{tech}</li>
-                  ))}
-                </ul>
                 <div
-                  className={`flex gap-2 justify-start ${
+                  className={`flex flex-col gap-4 p-7 md-p-0 relative col-[1_/_-1] ${
                     project.id % 2 === 0
-                      ? 'md:justify-start'
-                      : 'md:justify-end'
-                  }`}
+                      ? 'md:col-[1_/_9] lg:col-[1_/_7]'
+                      : 'md:col-[5_/_-1] lg:col-[7_/_-1]'
+                  } row-[1_/_-1] z-20`}
+                >
+                  <h2
+                    className={`flex justify-start ${
+                      project.id % 2 === 0
+                        ? 'md:justify-start'
+                        : 'md:justify-end'
+                    } text-3xl`}
+                  >
+                    <a
+                      href={project['live-version-link']['link-href']}
+                      className="hover:text-green focus:text-green "
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {project.name}
+                    </a>
+                  </h2>
+                  <div className="md:bg-ligth-navy py-5 md:px-4 min-h-fit text-slate-400">
+                    <p>{project.description}</p>
+                    {project.id === 1 || project.id === 2 ? (
+                      <span className="mt-3 text-sky-500">
+                        {`
+                        Login with Username: ${
+                          project.id === 2 ? 'solomon@gmail.com' : 'user2@gmail'
+                        } and password:
+                        admin123`}
+                      </span>
+                    ) : (
+                      ''
+                    )}
+                  </div>
+                  <ul
+                    className={`flex gap-2 flex-wrap justify-start ${
+                      project.id % 2 === 0
+                        ? 'md:justify-start'
+                        : 'md:justify-end'
+                    } text-slate-400"`}
+                  >
+                    {project.technologies.map((tech) => (
+                      <li key={tech}>{tech}</li>
+                    ))}
+                  </ul>
+                  <div
+                    className={`flex gap-2 justify-start ${
+                      project.id % 2 === 0
+                        ? 'md:justify-start'
+                        : 'md:justify-end'
+                    }`}
+                  >
+                    <a
+                      href={project['source-link']['source-href']}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img src={githubIcon} alt="Github" />
+                    </a>
+                    <a
+                      href={project['live-version-link']['link-href']}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <img src={liveIcon} alt="Live" />
+                    </a>
+                  </div>
+                </div>
+                <div
+                  className={`h-full opacity-25 md:opacity-50 hover:md:opacity-100 w-full md:bg-green bg-slate-200 relative col-[1_/_-1] ${
+                    project.id % 2 === 0 ? 'md:col-[6_/_-1]' : 'md:col-[1_/_8]'
+                  } row-[1_/_-1] z-10`}
                 >
                   <a
-                    href={project['source-link']['source-href']}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img src={githubIcon} alt="Github" />
-                  </a>
-                  <a
+                    className="h-full focus:md:opacity-100"
                     href={project['live-version-link']['link-href']}
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <img src={liveIcon} alt="Live" />
+                    <img
+                      className="h-full"
+                      src={images && images[project.id - 1]}
+                      alt={project.name}
+                    />
                   </a>
                 </div>
-              </div>
-              <div
-                className={`h-full opacity-25 md:opacity-50 hover:md:opacity-100 w-full md:bg-green bg-slate-200 relative col-[1_/_-1] ${
-                  project.id % 2 === 0 ? 'md:col-[6_/_-1]' : 'md:col-[1_/_8]'
-                } row-[1_/_-1] z-10`}
-              >
-                <a
-                  className="h-full focus:md:opacity-100"
-                  href={project['live-version-link']['link-href']}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    className="h-full"
-                    src={images && images[project.id - 1]}
-                    alt={project.name}
-                  />
-                </a>
-              </div>
-            </li>
-          ) : (
-            ''
-          )))}
+              </li>
+            ))}
         </ul>
       </div>
       <div>
@@ -130,56 +147,56 @@ const Works = () => {
           </h1>
         </div>
         <ul className="my-10 gap-2 grid md:grid-cols-2 lg:grid-cols-3">
-          {projects.map((project) => (project.id > 3 ? (
-            <a
-              key={project.id}
-              href={project['live-version-link']['link-href']}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex"
-            >
-              <li
-                className="wow fadeInUp flex flex-col justify-between gap-3 hover:scale-x-105 md:hover:scale-100 lg:hover:scale-y-110 bg-ligth-navy p-5"
-                data-wow-delay="0.5s"
-                data-wow-duration="2s"
+          {projects
+            .filter((project) => project.id > 4)
+            .map((project) => (
+              <a
+                key={project.id}
+                href={project['live-version-link']['link-href']}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex"
               >
-                <header className="flex flex-col gap-3">
-                  <div className="flex justify-between items-center pb-4">
-                    <img src={folderIcon} className="" alt="folder" />
-                    <div className="flex gap-2 justify-end">
-                      <a
-                        href={project['source-link']['source-href']}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <img src={githubIcon} alt="Github" />
-                      </a>
-                      <a
-                        href={project['live-version-link']['link-href']}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <img src={liveIcon} alt="Live" />
-                      </a>
+                <li
+                  className="wow fadeInUp flex flex-col justify-between gap-3 hover:scale-x-105 md:hover:scale-100 lg:hover:scale-y-110 bg-ligth-navy p-5"
+                  data-wow-delay="0.5s"
+                  data-wow-duration="2s"
+                >
+                  <header className="flex flex-col gap-3">
+                    <div className="flex justify-between items-center pb-4">
+                      <img src={folderIcon} className="" alt="folder" />
+                      <div className="flex gap-2 justify-end">
+                        <a
+                          href={project['source-link']['source-href']}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <img src={githubIcon} alt="Github" />
+                        </a>
+                        <a
+                          href={project['live-version-link']['link-href']}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <img src={liveIcon} alt="Live" />
+                        </a>
+                      </div>
                     </div>
-                  </div>
-                  <div className="flex justify-start text-2xl text-slate-300">
-                    <h2>{project.name}</h2>
-                  </div>
-                  <div>
-                    <p>{project.description}</p>
-                  </div>
-                </header>
-                <ul className="flex gap-2 flex-wrap justify-start text-slate-400">
-                  {project.technologies.map((tech) => (
-                    <li key={tech}>{tech}</li>
-                  ))}
-                </ul>
-              </li>
-            </a>
-          ) : (
-            ''
-          )))}
+                    <div className="flex justify-start text-2xl text-slate-300">
+                      <h2>{project.name}</h2>
+                    </div>
+                    <div>
+                      <p>{project.description}</p>
+                    </div>
+                  </header>
+                  <ul className="flex gap-2 flex-wrap justify-start text-slate-400">
+                    {project.technologies.map((tech) => (
+                      <li key={tech}>{tech}</li>
+                    ))}
+                  </ul>
+                </li>
+              </a>
+            ))}
         </ul>
       </div>
     </section>
